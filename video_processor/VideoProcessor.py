57b50b2e147527dcb19a03f64ad133c1a054ebac
Anna Korunska <akorunska@gmail.com>
2020-12-13 21:50:05 +0200

    parse emotions code from the cv camera output

--- video_processor/VideoProcessor.py
+++ video_processor/VideoProcessor.py
@@ -1,15 +1,16 @@
 import cv2
 from apscheduler.schedulers.blocking import BlockingScheduler
-from fer import FER
+from deepface import DeepFace
+# from fer import FER
 
 
 class VideoProcessor:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
         self.sched = BlockingScheduler()
-        self.detector = FER()
-        self.image = ""
-        self.data = ""
+        # self.detector = FER()
+        self.image = None
+        self.data = None
 
     def start_sheduler(self):
         self.sched.add_job(self.process_input, 'interval', seconds=5)
@@ -17,13 +18,16 @@ class VideoProcessor:
         # self.sched.add_job(self.process_input, 'interval', seconds=1)
 
     def process_input(self):
-        print("here")
         success, frame = self.cam.read()
         if not success:
             raise Exception("Failed to read from camera")
-        self.image = frame
-        print(self.detector.detect_emotions(frame))
-
+        # Get coordinates of faces from the image
+        try:
+            self.data = DeepFace.analyze(frame, actions=['emotion'])
+            print(type(self.data))
+        except ValueError:
+            self.data = None
+        print(self.data)
 
     def __del__(self):
         self.cam.release()
